9e35c2453dbe0c962bc5b2a2f59e5a5a7903fcde
hiimstevejin <jingle5695@gmail.com>
2025-06-22 00:54:51 -0500

    refactor: update NewsArticle model

--- backend/app/models/news.py
+++ backend/app/models/news.py
@@ -6,9 +6,13 @@ class NewsArticle(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(500))
+    summary = db.Column(db.String)
     source = db.Column(db.String(100))
+    banner_image = db.Column(db.String)
     published_at = db.Column(db.DateTime)
-    raw_sentiment = db.Column(db.JSON)  # AlphaVantage's original sentiment
+    overall_sentiment_score = db.Column(db.Integer)
+    overall_sentiment_label = db.Column(db.String)
+    ticker_sentiment = db.Column(db.JSON)
     bert_sentiment = db.Column(db.JSON)  # FinBERT analysis
     
     @property
--- backend/app/routes/news.py
+++ backend/app/routes/news.py
@@ -9,22 +9,26 @@ print("news.py loaded")
 
 @news_bp.route("/analyze", methods=["GET"])
 def analyze_market():
-    print("analyze_market endpoint called")  # Add this line
-    tickers = request.args.get("tickers", "GOOG").split(",")
+    print("analyze_market endpoint called") 
+    tickers = request.args.get("tickers", "AAPL").split(",")
     analyzer = NewsAnalyzer(current_app.config["NEWS_FETCH_KEY"])
     articles = analyzer.analyze_todays_news(tickers=tickers, days_ago=7,topics=None,limit=5)
 
     # Save to database
-    db.session.bulk_save_objects(articles)
-    db.session.commit()
+    # db.session.bulk_save_objects(articles)
+    # db.session.commit()
 
     return jsonify(
         [
             {
                 "title": a.title,
+                "summary": a.summary,
                 "source": a.source,
-                "timepublished": a.published_at,
-                "raw_sentiment": a.raw_sentiment,
+                "banner_image": a.banner_image,
+                "time_published": a.published_at,
+                "overall_sentiment_score": a.overall_sentiment_score,
+                "overall_sentiment_label": a.overall_sentiment_label,
+                "ticker_sentiment": a.ticker_sentiment,
                 "sentiment": a.bert_sentiment["sentiment"],
                 "confidence": a.bert_sentiment["confidence"],
             }
